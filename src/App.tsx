1431133c289b8aaa79b57349d6a1c872d3a1a637
future2022 <10383726+future2022@user.noreply.gitee.com>
2023-10-22 17:54:26 +0800

    passing data via props

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.css'
+import ListGroup1 from './components/ListGroup1'
 import ListGroup from './components/ListGroup'
 
 function App() {
+  const lists = ["北京","上海","广州","深圳"];
   return (
     <div>
-      <ListGroup/>
-      <ListGroup/>
+      <ListGroup lists={lists} heading='ListGroup'/>
+      <ListGroup1/>
     </div>
-  )
-}
+    )
+  }
+
 
 export default App
--- src/components/ListGroup1.tsx
+++ src/components/ListGroup1.tsx
@@ -1,16 +1,12 @@
 import { MouseEvent, useState } from "react";
 
-interface Props {
-    lists: string[];
-    heading: string;
-}
-
-function ListGroup({lists, heading}: Props) {
+function ListGroup() {
+  const lists = ["北京","上海","广州","深圳"];
   let [selectIndex, setSelectIndex] = useState(-1);
   const handleClick = (index:number) => setSelectIndex(index);
   return (
     <>
-      <h1>{heading}</h1>
+      <h1>ListGroup1</h1>
       <ul className="list-group">
         {lists.map((item, index) => (
           <li className= {selectIndex === index ? "list-group-item active" : "list-group-item"} key={item} onClick={()=>{setSelectIndex(index)}}>
