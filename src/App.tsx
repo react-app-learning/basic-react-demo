5125c73cbd93ecd5ceb84cf06e7793ce6d0a139f
future2022 <10383726+future2022@user.noreply.gitee.com>
2023-10-22 19:20:00 +0800

    passing children

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.css'
-import ListGroup from './components/ListGroup'
+import Alert from './components/Alert'
 
 function App() {
-  const lists = ["北京","上海","广州","深圳"];
-  const handleClick = (name: string) => (console.log(name));
   return (
     <div>
-      <ListGroup lists={lists} heading='ListGroup1' onSelectItem={handleClick}/>
-      <ListGroup lists={lists} heading='ListGroup2' onSelectItem={handleClick}/>
+      <Alert>HelloWorld</Alert>
     </div>
     )
   }
